416db92ffc3469c42e83e482cc52f3e1e2d77793
user <user@BPLAP62.local>
2024-07-01 11:42:51 +0100

    main - doppler adding project output

--- doppler/main.tf
+++ doppler/main.tf
@@ -11,8 +11,15 @@ provider "doppler" {
   doppler_token = var.doppler_token
 }
 
-resource "doppler_config" "configs" {
+resource "doppler_config" "this" {
   project     = "global"
   environment = var.environment
   name        = "${var.environment}_${var.slug}"
+}
+
+data "doppler_secrets" "this" {
+  config = doppler_config.this.id 
+}
+output "all_secrets" {
+  value = data.doppler_secrets.this.map
 }
